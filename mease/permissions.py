82d6e1b1665e2a59790a2e69269c26ad78ee71f7
Florian PAQUET <contact@florianpaquet.com>
2014-01-17 21:56:20 +0100

    Fixed permission decorator

--- mease/permissions.py
+++ mease/permissions.py
@@ -9,5 +9,7 @@ def require_permission(perm_func):
             if not perm_func(*args, **kwargs):
                 # TODO : Handle errors
                 print("Not allowed")
+                return
+            func(*args, **kwargs)
         return wrapper
     return decored
