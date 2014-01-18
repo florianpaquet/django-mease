40796cdbbd9358d3cf4f27983a012a559ed684dc
Florian PAQUET <contact@florianpaquet.com>
2014-01-18 04:08:52 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info[0] == 2:
 
 setup(
     name='django-mease',
-    version='0.1.5',
+    version='0.1.6',
     description="Mease: Websocket integration made easy for Django",
     url="https://github.com/florianpaquet/django-mease",
     author="Florian PAQUET",
