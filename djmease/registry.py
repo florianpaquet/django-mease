bd5878ced2a23704441177d339b2d7804133185f
Florian PAQUET <contact@florianpaquet.com>
2014-01-21 23:01:27 +0100

    Updated settings format

--- djmease/registry.py
+++ djmease/registry.py
@@ -3,9 +3,9 @@ from django.utils.module_loading import import_by_path
 
 from mease import Mease
 
-from .settings import GLOBAL_CONF
 from .settings import BACKEND_CLASS_PATH
-
+from .settings import BACKEND_CONF
 
 BACKEND_CLASS = import_by_path(BACKEND_CLASS_PATH)
-mease = Mease(BACKEND_CLASS, GLOBAL_CONF)
+
+mease = Mease(BACKEND_CLASS, BACKEND_CONF)
--- djmease/settings.py
+++ djmease/settings.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 
-GLOBAL_CONF = getattr(settings, 'MEASE', {})
-WEBSOCKET = GLOBAL_CONF.get('WEBSOCKET_SERVER', {})
+BACKEND_CLASS_PATH = getattr(
+    settings, 'MEASE_BACKEND_CLASS', 'mease.backends.redis.RedisBackend')
+BACKEND_CONF = getattr(settings, 'MEASE_BACKEND_CONFIG', {})
 
-BACKEND_CLASS_PATH = GLOBAL_CONF.get('BACKEND_CLASS', 'mease.backends.redis.RedisBackend')
+WEBSOCKET = getattr(settings, 'MEASE_WEBSOCKET_CONFIG', {})
 
 WEBSOCKET_PORT = WEBSOCKET.get('PORT', 9090)
 WEBSOCKET_AUTORELOAD = WEBSOCKET.get('AUTORELOAD', getattr(settings, 'DEBUG', False))
